25fd8d5ab13bb2ea71d0f1bf085e8b7243979f9a
jakariya0dev <jhalhadi@gmail.com>
2026-02-26 16:28:13 +0600

    ui/dashboard: sidebar added

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,10 +1,12 @@
 import Header from "@/components/dashboard/Header";
+import Sidebar from "@/components/dashboard/Sidebar";
 import React from "react";
 
 export default function layout({ children }) {
   return (
     <div className="flex h-screen overflow-hidden">
       {/* sidebar */}
+      <Sidebar />
       <div className="flex flex-col flex-1 overflow-auto">
         
         <div className="max-w-7xl mx-auto w-full">
---
+++ components/dashboard/Sidebar.jsx
@@ -0,0 +1,64 @@
+"use client"
+import { House, DollarSign, Settings, ShoppingBag, ShoppingCart, Mail, Users, Bell, Info, Icon, Menu } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { useState } from 'react';
+
+
+const Links = [
+    {
+        name: 'Home',
+        href: '/dashboard',
+        icon: House 
+    },
+    {
+        name: 'Orders',
+        href: '/dashboard/orders',
+        icon: ShoppingCart
+    },
+    {
+        name: 'Products',
+        href: '/dashboard/products',
+        icon: ShoppingBag
+    },
+    {
+        name: 'Users',
+        href: '/dashboard/users',
+        icon: Users
+    },
+    {
+        name: 'Settings',
+        href: '/dashboard/settings',
+        icon: Settings
+    },
+]
+
+export default function Sidebar() {
+    const pathname = usePathname();
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
+
+
+  return (
+    <div className={`relative z-10 transition-all duration-300 ease-in-out flex shrink-0 ${isSidebarExpanded ? 'w-72' : 'w-20'}`}>
+        <div className='h-full w-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f]'>
+            <div className='flex justify-between'>
+                {isSidebarExpanded && <h1 className='text-2xl font-semibold text-gray-100'>Ecom</h1>}
+                <button onClick={() => setIsSidebarExpanded(!isSidebarExpanded)} className='flex items-center gap-4 py-2 px-4 rounded-md hover:bg-[#2f2f2f]'>
+                    <Menu size={20} />
+                    
+                </button>
+            </div>
+            <nav className='mt-8 grow'>
+               {
+                Links.map((link, index) => (
+                    <Link href={link.href} key={index} className={`flex items-center gap-4 py-2 px-4 rounded-md hover:bg-[#2f2f2f] ${pathname === link.href ? 'bg-[#2f2f2f]' : ''}`}>
+                        <link.icon size={20} />
+                        {isSidebarExpanded && <span>{link.name}</span> }
+                    </Link>
+                ))
+               }
+            </nav>
+        </div>
+    </div>
+  )
+}
